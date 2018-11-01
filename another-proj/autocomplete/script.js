dfbb3e57aaded4a7819e99de32dbd5d22c5c71e5
Azat Ayupov <runor1@yandex.ru>
2018-11-01 14:48:17 +0500

    add bio

---
+++ another-proj/autocomplete/script.js
@@ -0,0 +1,197 @@
+'use strict';
+
+let clean = document.querySelector('#clean');
+let renderContainer = document.querySelector('#render-city');
+let inputCityId = renderContainer.querySelector('.input-city-id');
+let cities = [];
+let input = document.querySelector('.input-city');
+let lock = false;
+let noConnect;
+let validate;
+const popularCities = [
+  {
+    "Id": 5001,
+    "City": "Москва"
+  },
+  {
+    "Id": 5003,
+    "City": "Санкт-Петербург"
+  },
+  {
+    "Id": 4980,
+    "City": "Екатеринбург"
+  }
+]
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function validateCity(cities, evt) {
+  let foundCity = 0;
+  cities.forEach(function (city) {
+    if (city['Id'].toString() + ' ' + city['City'] == inputCityId.value + ' ' + evt.target.value) {
+      console.log(city['Id'].toString() + ' ' + city['City']);
+      foundCity += 1;
+    }
+    if (foundCity == 0) {
+      renderContainer.classList.add('render-city_error');
+    }
+    else {
+      if (renderContainer.classList.contains('render-city_error')) {
+        renderContainer.classList.remove('render-city_error');
+      }
+    }
+  })
+  console.log(foundCity);
+}
+
+function msgError() {
+  // clearTimeout(noConnect);
+  noConnect = setTimeout(function () {
+    renderContainer.querySelector('.rendered').innerHTML = 'Ой что-то пошло не так';
+  }, 5000)
+};
+
+
+function loadCities(evt) {
+  if (lock == false) {
+    fetch('./data.json')
+      .then(res => {
+        if (Math.random() > 0.5) {
+          throw new Error('данные не загружены, попробуйте повторить загрузку');
+        }
+        lock = true;
+        setTimeout(function () {
+          return res.json()
+            .then(res => {
+              cities = res;
+              console.log(cities);
+            })
+        }, getRandomInt(1000, 2000));
+      })
+      .catch(e => {
+        console.error(e);
+        msgError();
+        console.info('повторная загрузка будет запущена через 10 секунд');
+        setTimeout(function () {
+          input.click();
+        }, 10000)
+      });
+  }
+}
+
+function showPopularCities(evt) {
+  let render = document.createElement('div');
+  render.classList.add('rendered');
+  renderContainer.appendChild(render);
+  let list = document.createElement('div');
+  list.classList.add('rendered__contain');
+  let popularCitiesLabel = document.createElement('div');
+  popularCitiesLabel.classList.add('render-city__popular-label');
+  popularCitiesLabel.innerHTML = 'Популярные города';
+  list.appendChild(popularCitiesLabel);
+  popularCities.forEach(function (el) {
+    let li = document.createElement('div');
+    li.classList.add('rendered__element');
+    li.innerText = el['Id'] + ' ' + el['City'];
+    li.addEventListener('click', function () {
+      inputCityId.value = el['Id'];
+      input.value = el['City'];
+      clean.appendChild(render);
+      clean.innerHTML = '';
+      validateCity(cities, evt);
+    });
+    list.appendChild(li);
+  });
+  render.appendChild(list);
+}
+
+function showCities(evt) {
+  if (renderContainer.querySelector('.rendered')) {
+    clean.appendChild(renderContainer.querySelector('.rendered'));
+    clean.innerHTML = '';
+  }
+  let render = document.createElement('div');
+
+  render.classList.add('rendered');
+  renderContainer.appendChild(render);
+  render.innerHTML = '';
+  let result = '';
+  let length = document.createElement('div');
+  length.classList.add('rendered__length');
+
+  if (cities.length == 0) {
+    length.innerHTML = '<div class="loader"></div><div>Загрузка</div>';
+    msgError();
+  };
+  result = cities.filter(function (el) {
+    return el['Id'].toString().startsWith(evt.target.value) || el['City'].toLowerCase().startsWith(evt.target.value.toLowerCase());
+  });
+  let sliced = result.slice(0, 5);
+  let list = document.createElement('div');
+  list.classList.add('rendered__contain');
+  sliced.forEach(function (el) {
+    let li = document.createElement('div');
+    li.classList.add('rendered__element');
+    li.innerText = el['Id'] + ' ' + el['City'];
+    li.addEventListener('click', function () {
+      inputCityId.value = el['Id'];
+      input.value = el['City'];
+      clean.appendChild(render);
+      clean.innerHTML = '';
+      validateCity(cities, evt);
+    });
+    list.appendChild(li);
+  });
+  if (result.length == 1) {
+    if (result[0]['Id'].toString() == evt.target.value || result[0]['City'].toLowerCase() == evt.target.value.toLowerCase()) {
+      evt.target.value = result[0]['City'];
+      inputCityId.value = result[0]['Id'];
+    }
+  }
+  if (cities.length > 0) {
+    length.innerText = 'Показано ' + sliced.length + ' из ' + result.length + ' найденных городов';
+  }
+  if (result.length == 0 && cities.length > 0) {
+    length.innerHTML = '<div>Не найдено</div>';
+  }
+  render.appendChild(list);
+  list.appendChild(length);
+  if (this.value == 0) {
+    clean.appendChild(render);
+    clean.innerHTML = '';
+    // showPopularCities(evt);
+  }
+  clearTimeout(validate);
+  validate = setTimeout(validateCity, 2000, cities, evt);
+}
+
+input.addEventListener('click', loadCities);
+
+input.addEventListener('click', function (evt) {
+  showPopularCities(evt);
+})
+
+input.addEventListener('input', showCities);
+
+input.onfocus = function () {
+  this.select();
+}
+
+input.onblur = function () {
+  console.log('test');
+  setTimeout(function () {
+    try {
+      clean.appendChild(renderContainer.querySelector('.rendered'));
+      clean.innerHTML = '';
+    }
+    catch (err) {
+
+    }
+  }, 100);
+}
+
+input.addEventListener('error', function (evt) {
+  console.log('error');
+})
---
+++ precache-manifest.76550afd40f243926a42a64011f441a9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "faccf5c490e1b71dc5c2dbde54c725f7",
+    "url": "/static/media/bold-italic.faccf5c4.ttf"
+  },
+  {
+    "revision": "999c46af3994d9f57a55",
+    "url": "/static/css/main.59f86ffc.chunk.css"
+  },
+  {
+    "revision": "87e17980018bc8885615",
+    "url": "/static/js/1.87e17980.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5d4adffb6b244532a12c78f4124af70",
+    "url": "/static/media/bold.b5d4adff.ttf"
+  },
+  {
+    "revision": "fb4c7f5813a0ec14f77da2fb523e3516",
+    "url": "/static/media/medium.fb4c7f58.ttf"
+  },
+  {
+    "revision": "999c46af3994d9f57a55",
+    "url": "/static/js/main.999c46af.chunk.js"
+  },
+  {
+    "revision": "6bb9beb4ae22b811bfd5369d517e2f7c",
+    "url": "/static/media/regular.6bb9beb4.ttf"
+  },
+  {
+    "revision": "669a64a5df65f7d143c69c09324e926c",
+    "url": "/static/media/medium-italic.669a64a5.ttf"
+  },
+  {
+    "revision": "0eb859e50b085c8ee96661b38083e515",
+    "url": "/static/media/italic.0eb859e5.ttf"
+  },
+  {
+    "revision": "97f53335662a79edbe3a2dbd0aa12892",
+    "url": "/static/media/bg.97f53335.jpg"
+  },
+  {
+    "revision": "4db5bbe6d296301b55e4340a85bc4936",
+    "url": "/static/media/ava.4db5bbe6.png"
+  },
+  {
+    "revision": "53a019f345184c09ab1ee2587e129473",
+    "url": "/index.html"
+  }
+];
